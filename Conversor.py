1eaf77d14008b25981ec0955dfe7db57ef68066d
0neLuffy <ricardo.monr@outlook.com>
2023-09-13 11:42:34 -0500

    Actualizar Conversor.py y archivos relacionados

--- Conversor.py
+++ Conversor.py
@@ -1,20 +1,40 @@
 from pdf2docx import Converter
 from docx2pdf import convert
 
-# Definición de función para convertir un archivo PDF a Word.
-
 
 def pdf_to_docx(pdf_file, docx_file):
-    # Crea un objeto 'Converter' para convertir el PDF en Word.
-    cv = Converter(pdf_file)
-    # Realiza la conversión y guarda el resultado en un archivo Word.
-    cv.convert(docx_file, start=0, end=None)
-    # Cierra el objeto Converter después de la conversión.
-    cv.close()
+    """
+    Convierte un archivo PDF en un archivo Word (.docx).
 
-# Definición de función para convertir un archivo Word a PDF.
+    Args:
+        pdf_file (str): Ruta al archivo PDF de origen.
+        docx_file (str): Ruta al archivo Word (.docx) de destino.
+
+    Raises:
+        Exception: Se lanza si ocurre un error durante la conversión.
+    """
+    try:
+        # Crea un objeto 'Converter' para convertir el PDF en Word.
+        cv = Converter(pdf_file)
+        # Realiza la conversión y guarda el resultado en un archivo Word.
+        cv.convert(docx_file, start=0, end=None)
+    except Exception as e:
+        raise e
+    finally:
+        # Cierra el objeto Converter después de la conversión.
+        cv.close()
 
 
 def docx_to_pdf(docx_file, pdf_file):
-    # Utiliza la función 'convert' de 'docx2pdf' para realizar la conversión.
-    convert(docx_file, pdf_file)
+    """
+    Convierte un archivo Word (.docx) en un archivo PDF.
+
+    Args:
+        docx_file (str): Ruta al archivo Word (.docx) de origen.
+        pdf_file (str): Ruta al archivo PDF de destino.
+    """
+    try:
+        # Utiliza la función 'convert' de 'docx2pdf' para realizar la conversión.
+        convert(docx_file, pdf_file)
+    except Exception as e:
+        raise e
